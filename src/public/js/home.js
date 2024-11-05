3aa091851436c2556a9dbbdcd857a8ea1dddf9ee
pabloj <pablojanb@gmail.com>
2024-11-05 15:19:22 -0300

    resuelvo script home para renderizar link de cart dinamico

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,6 +1,8 @@
 const socket = io()
 
 const addToCartBtns = document.querySelectorAll(".add-to-cart-btn")
+const link_cart = document.getElementById("link_cart")
+
 let newCart = JSON.parse(localStorage.getItem('cart')) || null
 
 if (!newCart) {
@@ -10,6 +12,9 @@ if (!newCart) {
 socket.on('cart', cart=>{
     newCart = cart
     localStorage.setItem('cart', JSON.stringify(newCart))
+    link_cart.innerHTML = `
+                        <a href="http://localhost:8080/carts/${newCart._id}">Cart</a>
+                        `
 })
 
 addToCartBtns.forEach(btn=>{
@@ -18,3 +23,8 @@ addToCartBtns.forEach(btn=>{
         socket.emit('add-to-cart', {prodId, newCart})
     })
 })
+
+
+link_cart.innerHTML = `
+                        <a href="http://localhost:8080/carts/${newCart._id}">Cart</a>
+                        `
